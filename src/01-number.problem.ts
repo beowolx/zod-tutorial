b285aee2f31def9646b7d0b69c7b1cc5b50db0b2
beowulf <61982523+beowolx@users.noreply.github.com>
2023-02-07 15:28:23 +0100

    feat: did exercise 01

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -4,18 +4,20 @@ import { expect, it } from "vitest";
 import { z } from "zod";
 //       ^ 🕵️‍♂️
 
+const numParser = z.number().transform((n: number) => String(n));
+
 export const toString = (num: unknown) => {
-  return String(num);
+    return numParser.parse(num)
 };
 
 // TESTS
 
 it("Should throw a runtime error when called with not a number", () => {
-  expect(() => toString("123")).toThrowError(
-    "Expected number, received string",
-  );
+    expect(() => toString("123")).toThrowError(
+        "Expected number, received string",
+    );
 });
 
 it("Should return a string when called with a number", () => {
-  expect(toString(1)).toBeTypeOf("string");
+    expect(toString(1)).toBeTypeOf("string");
 });
