9e5efdfb6c94823b261d58cb68b1df18ff54d013
beowulf <61982523+beowolx@users.noreply.github.com>
2023-02-07 15:34:46 +0100

    feat: did exercise 02

--- src/02-object.problem.ts
+++ src/02-object.problem.ts
@@ -3,22 +3,28 @@
 import { expect, it } from "vitest";
 import { z } from "zod";
 
-const PersonResult = z.unknown();
+const personResultParser = z.object(
+    {
+        name: z.string().max(100),
+    }
+);
+
+type PersonResult = z.infer<typeof personResultParser>
 //                   ^ 🕵️‍♂️
 
-export const fetchStarWarsPersonName = async (id: string) => {
-  const data = await fetch(
-    "https://www.totaltypescript.com/swapi/people/" + id + ".json",
-  ).then((res) => res.json());
+export const fetchStarWarsPersonName = async (id: string): Promise<PersonResult["name"]> => {
+    const data = await fetch(
+        "https://www.totaltypescript.com/swapi/people/" + id + ".json",
+    ).then((res) => res.json());
 
-  const parsedData = PersonResult.parse(data);
+    const parsedData = personResultParser.parse(data);
 
-  return parsedData.name;
+    return parsedData.name;
 };
 
 // TESTS
 
 it("Should return the name", async () => {
-  expect(await fetchStarWarsPersonName("1")).toEqual("Luke Skywalker");
-  expect(await fetchStarWarsPersonName("2")).toEqual("C-3PO");
+    expect(await fetchStarWarsPersonName("1")).toEqual("Luke Skywalker");
+    expect(await fetchStarWarsPersonName("2")).toEqual("C-3PO");
 });
